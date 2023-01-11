ace0c52929a88790edaa3db139bf2d5c9a3b2474
Valentin <V.Kovalenko.prog@gmail.com>
2023-01-11 13:37:19 +0200

    add Auth component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-
+import { useState, useRef } from "react";
+import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+import { Chat } from "./components/Chat";
+const cookies = new Cookies();
 
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState("");
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <div className="App">
+        <Auth setIsAuth={setIsAuth}/>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <h1>Hello!!!</h1>
+    <div>
+      {room ? (
+        <Chat/>
+      ) : (
+        <div className="room">
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )}
     </div>
   );
 }
